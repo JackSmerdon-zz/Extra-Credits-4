0d8d18ff5bc6d7264c9b3c90e58a2920bc80f8ee
WilliamAkins <w.akins111@gmail.com>
2019-05-31 00:52:01 +0100

    main stuff to make a basic game work

--- Source/ExtraCreditsProject/ExtraCreditsProjectGameModeBase.cpp
+++ Source/ExtraCreditsProject/ExtraCreditsProjectGameModeBase.cpp
@@ -2,4 +2,16 @@
 
 
 #include "ExtraCreditsProjectGameModeBase.h"
+#include "UObject/ConstructorHelpers.h"
 
+AExtraCreditsProjectGameModeBase::AExtraCreditsProjectGameModeBase()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameMode now running"));
+
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Core/MainCharacterBP"));
+
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
